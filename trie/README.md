0b95c351d6425e56cb3838482899d416d39d726e
Om Balapure <ombalapure@Oms-MacBook-Air.local>
2025-05-04 16:09:25 +0530

    Trie

---
+++ trie/README.md
@@ -0,0 +1,28 @@
+## Trie
+
+It is a trie data structure that can be used to find words, given a prefix. It can be done using `O(w)`, where w is the length of the word.
+
+## Complexity
+- Add and search words.
+- Replace words in a sentence.
+- Design search autocomplete.
+
+### Time
+
+| Opeartion | Time |
+|-----------|------|
+| insert    | O(w) |
+| search    | O(w) |
+| starts_with   | O(w) |
+
+### Space
+
+| Opeartion | Time |
+|-----------|------|
+| insert    | O(1) |
+| search    | O(1) |
+| starts_with   | O(1) |
+
+> **w**: length of the word of prefix   
+> **n**: total no. of words  
+> Total space take will be `O(n * w)`
---
+++ trie/trie.py
@@ -0,0 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Insert a word
+        Time: O(w)
+        Space: O(w)
+        NOTE: `TrieNode` is not counted as part of the auxiliary space
+        """
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+
+    def search(self, word):
+        """
+        Search a complete word
+        Time: O(w)
+        Space: O(1)
+        NOTE: `TrieNode` is not counted as part of the auxiliary space
+        """
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.word
+
+    def starts_with(self, prefix):
+        """
+        Search for a prefix
+        Time: O(w)
+        Space: O(1)
+        NOTE: `TrieNode` is not counted as part of the auxiliary space
+        """
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
